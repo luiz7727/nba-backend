5ba5bbe1a6137755da8c5c6f96944b0af239a66e
luiz7727 <luiz.antonio999@hotmail.com>
2022-01-11 07:42:49 -0300

    Firt commit

---
+++ src/main/java/com/example/nbabackend/NbaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbaBackendApplication.class, args);
+    }
+
+}
