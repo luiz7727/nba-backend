7f391a719abcbabca485ec19d473456ebc9364c6
luiz7727 <luiz.antonio999@hotmail.com>
2022-01-11 09:20:32 -0300

    Class User model

---
+++ src/main/java/com/example/nbabackend/Model/User.java
@@ -0,0 +1,34 @@
+package com.example.nbabackend.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+
+    private String nickname;
+    private String senha;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
