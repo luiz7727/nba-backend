6bad0b073aad99fcb62738c2787919c4cc60300a
luiz7727 <luiz.antonio999@hotmail.com>
2022-01-11 22:52:05 -0300

    Card Model to user create a fake credit card at app

--- src/main/java/com/example/nbabackend/Model/Card.java
+++ src/main/java/com/example/nbabackend/Model/Card.java
@@ -1,2 +1,33 @@
-package com.example.nbabackend.Model;public class Card {
+package com.example.nbabackend.Model;
+
+public class Card {
+
+    private String cardName;
+    private int cardNumber;
+    private int cardCvv;
+
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getCardCvv() {
+        return cardCvv;
+    }
+
+    public void setCardCvv(int cardCvv) {
+        this.cardCvv = cardCvv;
+    }
 }
