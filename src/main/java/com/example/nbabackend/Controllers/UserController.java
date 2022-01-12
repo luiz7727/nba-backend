76174a1cd587d6835b7571cfff93bbd85f6a2661
luiz7727 <luiz.antonio999@hotmail.com>
2022-01-11 22:50:20 -0300

    Created a new route to see if the user already has a nickname into database and a new route to see all users

--- src/main/java/com/example/nbabackend/Controllers/UserController.java
+++ src/main/java/com/example/nbabackend/Controllers/UserController.java
@@ -3,8 +3,11 @@ package com.example.nbabackend.Controllers;
 import com.example.nbabackend.Model.User;
 import com.example.nbabackend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -13,6 +16,14 @@ public class UserController {
     @Autowired
     UserService _userService;
 
+
+    //Get All Users
+    @GetMapping("/getAll")
+    public List<User> getAllUsers (){
+
+        return _userService.getAllUsers();
+    }
+
     //GetById
     @GetMapping("/getUserId/{nickname}")
     public ResponseEntity<User> getUserById(@PathVariable String nickname){
@@ -20,11 +31,16 @@ public class UserController {
 
     }
 
-
     //createUser
     @PostMapping("/createUser")
     public User createUser(@RequestBody User user){
         return _userService.createNewUser(user);
+    }
+
+    //User Registreded
+    @PostMapping("/userRegistred")
+    public HttpStatus userCreated(@RequestBody User user){
+        return _userService.userAlreadyCreated(user);
     }
 
 }
