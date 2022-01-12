64987570a4f0f73fb97b69bbc7c090fa3e99506d
luiz7727 <luiz.antonio999@hotmail.com>
2022-01-11 22:52:40 -0300

    extended Model Card

--- src/main/java/com/example/nbabackend/Model/User.java
+++ src/main/java/com/example/nbabackend/Model/User.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User {
+public class User extends Card{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,4 +31,33 @@ public class User {
         this.senha = senha;
     }
 
+    @Override
+    public int getCardCvv() {
+        return super.getCardCvv();
+    }
+
+    @Override
+    public void setCardCvv(int cardCvv) {
+        super.setCardCvv(cardCvv);
+    }
+
+    @Override
+    public int getCardNumber() {
+        return super.getCardNumber();
+    }
+
+    @Override
+    public void setCardNumber(int cardNumber) {
+        super.setCardNumber(cardNumber);
+    }
+
+    @Override
+    public String getCardName() {
+        return super.getCardName();
+    }
+
+    @Override
+    public void setCardName(String cardName) {
+        super.setCardName(cardName);
+    }
 }
